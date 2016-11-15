8eb87561085534faeb49df73da51e6c5c28340fe
ekiminatorn <emil@bladenode.net>
2016-11-15 20:44:09 +0200

    Ping google to test host connectivity

--- monitor.py
+++ monitor.py
@@ -36,7 +36,7 @@ class MonitorPool:
 	def handle_change(self, thread_name, check_id, check_name, lock_uid, status, check_result):
 		if status == 'offline':
 			import subprocess
-			googleResult = subprocess.Popen(['ping', '95.85.8.15', '-c', '3', '-w', '3'], stdout=subprocess.PIPE).stdout.read()
+			googleResult = subprocess.Popen(['ping', 'ipv4.google.com', '-c', '3', '-w', '3'], stdout=subprocess.PIPE).stdout.read()
 			if '100% packet loss' in googleResult:
 				print "What?! Google down? Aborting alert sending as it seems like the monitor host has no connectivity"
 				return
